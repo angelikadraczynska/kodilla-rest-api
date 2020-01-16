d4b6b3d37b156830b1efe95ed018c96c015e9d44
Angelika Draczynska <nayrissa@outlook.com>
2020-01-16 13:23:14 +0100

    modify server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('express-cors');
+const path = require('path');
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -9,6 +10,7 @@ app.use(cors({
       'localhost:3000',
   ]
 }))
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -18,10 +20,14 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
