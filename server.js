49c07ea80412424b2e3ebe7aec950d62321f21e0
Angelika Draczynska <nayrissa@outlook.com>
2020-01-21 13:41:13 +0100

    init socket.io

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('express-cors');
 const path = require('path');
+const socket = require('socket.io');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
@@ -28,7 +30,12 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
-  console.log();
+});
+
+const io = socket(server);
+
+io.on('connection', socket => {
+  console.log('New socket! Its id – ' + socket.id);
 });
